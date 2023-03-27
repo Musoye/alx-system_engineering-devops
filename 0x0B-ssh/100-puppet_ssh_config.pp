8b1bda2d16e81633f4806bdabcccf5e0a9a03bbb
Musoye <oyebamijimustapha44@gmail.com>
2023-03-27 09:54:34 -0700

    edit syatem

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -4,14 +4,14 @@ include stdlib
 file_line { 'Turn off passwd':
   ensure  => present,
   path    => '/etc/ssh/ssh_config',
-  line    => '     PasswordAuthentication no'
+  line    => '     PasswordAuthentication no',
   replace => true,
 }
 
 file_line { 'identity change':
   ensure  => present,
   path    => '/etc/ssh/ssh_config',
-  line    => '     IdentityFile ~/.ssh/school'
+  line    => '     IdentityFile ~/.ssh/school',
   replace => true,
 }
 
