dfc4c8f164a61bb5f5641680553064a9c231b8b0
Musoye <oyebamijimustapha44@gmail.com>
2023-03-27 09:37:58 -0700

    puppet config ssh

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,17 @@
+# edit config file
+include stdlib
+
+file_line { 'Turn off passwd':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '     PasswordAuthentication no'
+  replace => true,
+}
+
+file_line { 'identity change':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '     IdentityFile ~/.ssh/school'
+  replace => true,
+}
+
