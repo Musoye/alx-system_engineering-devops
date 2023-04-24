df8a3e1ed75b2ca4cc99454eb314f19c96e90cee
oyebamiji mustapha <oyebamijimustapha44@gmail.com>
2023-04-25 00:30:21 +0100

    feat: get information of all users

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""import api from a url"""
+import json
+import requests
+
+if __name__ == "__main__":
+    data = {}
+    for i in range(1,11):
+        id = i
+        url = "https://jsonplaceholder.typicode.com"
+        res = requests.get('{}/users/{}'.format(url, id)).json()
+        res_todo = requests.get("{}/todos".format(url),
+                                params={"userId": id}).json()
+        name = res.get("username")
+        user_data = list(map(
+                    lambda x: {
+                        "task": x.get("title"),
+                        "completed": x.get("completed"),
+                        "username": name
+                    },
+                    res_todo
+                ))
+        data["{}".format(id)] = user_data
+with open('todo_all_employees.json', 'w') as apidata:
+    json.dump(data, apidata)
