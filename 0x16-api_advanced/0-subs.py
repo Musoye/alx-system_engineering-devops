f8e40a8fcd0fbbc605fa75e05a8ac75dc95f01a1
Musoye <oyebamijimustapha44@gmail.com>
2023-05-09 22:12:56 +0100

    1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,9 +6,13 @@ import sys
 
 def number_of_subscribers(subreddit):
     """number of suscribers"""
-    sus - sys.argv[1]
-    if sus is None or type
+    if subreddit is None or type(subreddit) != str:
+        return (0)
     url = "https://reddit.com/r/{}/about.json".format(
-            sys.argv[1])
+            subreddit)
     headers = {'User-Agent': 'API project'}
     response = requests.get(url, headers=headers, allow_redirects=False)
+    if (response.status_code != 200):
+        return (0)
+    sus = response.json.get('suscribers', 0)
+    return (sus)
