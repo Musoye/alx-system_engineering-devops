db99abe3b1979a120d8a899a479bcb32de402750
Musoye <oyebamijimustapha44@gmail.com>
2023-05-10 01:01:41 +0100

    feat: get suscriber from reddit API

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
 """get a lit of suscribber on a subreddit"""
 import requests
-import sys
 
 
 def number_of_subscribers(subreddit):
     """number of suscribers"""
     if subreddit is None or type(subreddit) != str:
         return (0)
-    url = "https://reddit.com/r/{}/about.json".format(
+    url = "http://www.reddit.com/r/{}/about.json".format(
             subreddit)
     headers = {'User-Agent': 'API project'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    response = requests.get(url, headers=headers)
     if (response.status_code != 200):
         return (0)
-    sus = response.json.get('suscribers', 0)
+    sus = response.json().get("data").get('subscribers', 0)
     return (sus)
